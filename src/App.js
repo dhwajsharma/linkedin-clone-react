eaeb63353c711bffb774af9b33565cb2f5084af5
Dhwaj Sharma <dhwajsharma10@gmail.com>
2021-02-27 09:42:29 +0530

    working on Sidebar

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header/Header';
+import Sidebar from './components/Sidebar/Sidebar'
+// import Feed from './components/Feed/Feed'
+// import Widgets from './components/Widgets/Widgets'
+
 
 function App() {
   return (
     <div className="App">
-
       <Header />
-      {/* App body */}
-
-      {/* Sidebar */}
-
-      {/* Feed */}
-
-      {/* Widgets */}
+      <div className="app_body">
+        <Sidebar />
+        {/* <Feed /> */}
+        {/* <Widgets /> */}
+      </div>
     </div>
   );
 }
