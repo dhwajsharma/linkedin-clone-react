b1bc447dd7e8101f22c38fd1b9d5b3ed98cd8ac0
Dhwaj Sharma <dhwajsharma10@gmail.com>
2021-03-22 20:30:18 +0530

    Building Input Option

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import Feed from './components/Feed/Feed';
 import Header from './components/Header/Header';
 import Sidebar from './components/Sidebar/Sidebar'
 // import Feed from './components/Feed/Feed'
--- src/components/Feed/Feed.js
+++ src/components/Feed/Feed.js
@@ -1,10 +1,22 @@
 import React from 'react'
 import "./Feed.css"
+import CreateIcon from "@material-ui/icons/Create"
 
 const Feed = () => {
     return (
-        <div>
+        <div className="feed">
+            <div className="feed_inputContainer">
+                <div className="feed_input">
+                    <CreateIcon />
+                    <form>
+                        <input type="text" />
+                        <button type="submit">Send</button>
+                    </form>
+                </div>
+                <div className="feed_inputOptions">
 
+                </div>
+            </div>
         </div>
     )
 }
