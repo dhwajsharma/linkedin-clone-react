9050224d18180049e32a9e2243c82d6be15bd119
Dhwaj Sharma <dhwajsharma10@gmail.com>
2021-03-22 18:14:23 +0530

    done with Sidebar component

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,9 +1,40 @@
+import { Avatar } from '@material-ui/core'
 import React from 'react'
+import "./Sidebar.css"
 
 const Sidebar = () => {
-    return (
-        <div>
 
+    const recentItem = (topic) => (
+        <div className="sidebar_recentItem">
+            <span className="sidebar_hash">#</span>
+            <p>{topic}</p>
+        </div>
+    )
+    return (
+        <div className="sidebar">
+            <div className="sidebar_top">
+                <img src="https://images.unsplash.com/photo-1616144848810-11eec385ddbd?ixid=MXwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw1fHx8ZW58MHx8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=700&q=60" alt="" />
+                <Avatar className="sidebar_avatar" />
+                <h2>Dhwaj Sharma</h2>
+                <h4>dhwajsharma10@gmail.com</h4>
+            </div>
+
+            <div className="sidebar_stats">
+                <div className="sidebar_stat">
+                    <p>Who viewed your profile</p>
+                    <p className="sidebar_statNumber">707</p>
+                </div>
+                <div className="sidebar_stat">
+                    <p>Connections</p>
+                    <p className="sidebar_statNumber">2,628</p>
+                </div>
+            </div>
+            <div className="sidebar_bottom">
+                <p>Recent</p>
+                {recentItem("reactjs")}
+                {recentItem("programming")}
+                {recentItem("jobs")}
+            </div>
         </div>
     )
 }
