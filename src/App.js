2309ca49ba2e4613768dec5c515dca86c8d66263
Dhwaj Sharma <dhwajsharma10@gmail.com>
2021-03-23 21:33:59 +0530

    Integrated Redux

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import './App.css';
 import Feed from './components/Feed/Feed';
 import Header from './components/Header/Header';
 import Sidebar from './components/Sidebar/Sidebar'
+import { selectUser } from './features/userSlice';
 // import Feed from './components/Feed/Feed'
 // import Widgets from './components/Widgets/Widgets'
 
 
 function App() {
+
+  const user = useSelector(selectUser)
+
   return (
     <div className="App">
       <Header />
