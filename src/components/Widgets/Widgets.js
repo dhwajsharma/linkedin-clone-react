5582a0a10e5b15c8922fe6d7d78a8ee0b611b762
Dhwaj Sharma <dhwajsharma10@gmail.com>
2021-03-24 17:41:34 +0530

    done with Widgets section.

--- src/components/Widgets/Widgets.js
+++ src/components/Widgets/Widgets.js
@@ -1,11 +1,35 @@
+import { FiberManualRecord, Info } from '@material-ui/icons'
 import React from 'react'
 import "./Widgets.css"
 
 const Widgets = () => {
-    return (
-        <div>
+
+    const newsArticle = (heading, subtitle) => (
+        <div className="widgets_article">
+
+            <div className="widgets_articleLeft">
+                <FiberManualRecord />
+            </div>
+
+            <div className="widgets_articleRight">
+                <h4>{heading}</h4>
+                <p>{subtitle}</p>
+            </div>
 
         </div>
+    )
+
+    return (
+        <div className="widgets">
+            <div className="widgets_banner">
+                <h2>LinkedIn News</h2>
+                <Info />
+            </div>
+            {newsArticle("A hiring binge in IT is coming.", "Top news - 2000 readers")}
+            {newsArticle("Second wave hits hiring speed.", "Top news - 1890 readers")}
+
+        </div>
+
     )
 }
 
